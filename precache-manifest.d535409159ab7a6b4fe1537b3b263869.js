4ae66eece4a9d8581929b2172dfe74c645aaed70
sbj2 <sbj@ryencatchers.com>
2022-06-20 23:06:42 +0900

    Updates

---
+++ precache-manifest.d535409159ab7a6b4fe1537b3b263869.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90750b8c648372629fcbe879be339150",
+    "url": "/sbportfolio/index.html"
+  },
+  {
+    "revision": "d2cde558c1eb7283ceae",
+    "url": "/sbportfolio/static/css/main.1ccc4bb8.chunk.css"
+  },
+  {
+    "revision": "45d28eeeae8305f4b439",
+    "url": "/sbportfolio/static/js/2.c1731ff1.chunk.js"
+  },
+  {
+    "revision": "f367b23accb7f1ea3a31ec366e6e9c88",
+    "url": "/sbportfolio/static/js/2.c1731ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2cde558c1eb7283ceae",
+    "url": "/sbportfolio/static/js/main.418331d1.chunk.js"
+  },
+  {
+    "revision": "018fcff3605fe2837519",
+    "url": "/sbportfolio/static/js/runtime-main.18244425.js"
+  },
+  {
+    "revision": "f8228ca3cea0c4226e69d8daa82ba2a0",
+    "url": "/sbportfolio/static/media/airbnb.f8228ca3.gif"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/sbportfolio/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/sbportfolio/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "c09e4c5a015f1bca3b6ba1ac8f87cec7",
+    "url": "/sbportfolio/static/media/channel.c09e4c5a.png"
+  },
+  {
+    "revision": "d6c4e78d7090519fda9f11d208356f79",
+    "url": "/sbportfolio/static/media/up-arrow-circle.d6c4e78d.svg"
+  }
+]);
