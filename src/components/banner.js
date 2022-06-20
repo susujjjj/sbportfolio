7540eda5eb519d8d2cabb41d385210e8bc5edaf1
sbj2 <sbj@ryencatchers.com>
2022-06-20 23:25:39 +0900

    fix: About me Link수정

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ReactComponent as RightArrow } from "../assets/arrow-right.svg";
+import { NavLink } from "react-router-dom";
 
 const Banner = () => {
   return (
@@ -15,7 +16,9 @@ const Banner = () => {
             </div>
           </h2>
           <div className="btn-row">
-            <a href="/about">More about me<RightArrow /></a>
+            <NavLink to="/about" exact>
+              More About me<RightArrow />
+            </NavLink>
           </div>
         </div>
       </div>
--- src/components/header.js
+++ src/components/header.js
@@ -1,62 +1,8 @@
-// import React, { useEffect, useState } from "react";
-// import { NavLink } from "react-router-dom";
-// import { withRouter } from "react-router-dom";
-// import { ReactComponent as UpArrow } from "../assets/up-arrow-circle.svg";
-// import { openMenu, closeMenu } from "../animations/menuAnimations";
-
-// // Define reducer
-
-// const Header = ({ history, dimensions }) => {
-//   // const [menuState, setMenuState] = useState({ menuOpened: false });
-//   // useEffect(() => {
-//   //   //Listening for page changes.
-//   //   history.listen(() => {
-//   //     setMenuState({ menuOpened: false });
-//   //   });
-//   //   if (menuState.menuOpened === true) {
-//   //     openMenu(dimensions.width);
-//   //   } else if (menuState.menuOpened === false) {
-//   //     closeMenu();
-//   //   }
-//   // });
-
-//   return (
-//     <div className='header'>
-//       <div className='container'>
-//         <div className='row v-center space-between'>
-//           <div className='logo'>
-//             <NavLink to='/' exact>
-//               AGENCY
-//             </NavLink>
-//           </div>
-//           <div className='nav-toggle'>
-//             <div
-            
-//               className='hamburger-menu'>
-//               <span></span>
-//               <span></span>
-//             </div>
-//             <div
-//               className='hamburger-menu-close'
-//               // onClick={() => setMenuState({ menuOpened: false })}
-//             >
-//               <UpArrow />
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-// export default withRouter(Header);
-
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { withRouter } from "react-router-dom";
 import { ReactComponent as UpArrow } from "../assets/up-arrow-circle.svg";
 import { openMenu, closeMenu } from "../animations/menuAnimations";
-
-// Define reducer
 
 const Header = ({ history, dimensions }) => {
   const [menuState, setMenuState] = useState({ menuOpened: false });
