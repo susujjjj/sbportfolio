f6ed4c9be7f5833eb5b3ede09488ab7edbe626ee
sbj2 <sbj@ryencatchers.com>
2022-08-26 17:22:53 +0900

    link 변경

--- src/components/header.js
+++ src/components/header.js
@@ -24,7 +24,7 @@ const Header = ({ history, dimensions }) => {
       <div className="container">
         <div className="row v-center space-between">
           <div className="logo">
-            <NavLink to="/" exact>
+            <NavLink to="/sbportfolio" exact>
               Chloe's Portfolio
             </NavLink>
           </div>
